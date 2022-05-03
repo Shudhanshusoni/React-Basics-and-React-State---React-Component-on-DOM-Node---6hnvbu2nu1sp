0aae945c783a668be81bf21587716ab7c32108bd
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-04 03:37:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDom.render (<App/>,Document.getElementById("root"));
+ReactDOM.render (<App/>,Document.getElementById("root"));
 
